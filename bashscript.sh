25dc205df6189750cfd2d2b0db74eaa019861217
Mohammad Bayat <OkBayat@gmail.com>
2022-08-02 15:59:13 +0430

    Update bashscript.sh

--- bashscript.sh
+++ bashscript.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
 
+echo ""
+echo "--------------------------------------------------"
+echo ">>> Setting vim color them"
+echo "--------------------------------------------------"
 mkdir ~/.vim/colors
 cp darcula.vim ~/.vim/colors/
-cp .ctags ~/ 
+echo "DONE"
+
+
+echo ""
+echo "--------------------------------------------------"
+echo ">>> Copy .ctags file to root"
+echo "--------------------------------------------------"
+cp .ctags ~/
+echo "DONE"
+
+
+echo ""
+echo "--------------------------------------------------"
+echo ">>> Installing zsh"
+echo "--------------------------------------------------"
+apt-get update
+apt-get install zsh
+zsh --version
+# Making ZSH the Default Shell
+usermod -s /usr/bin/zsh $(whoami)
+echo "DONE"
+
+echo ""
+echo "--------------------------------------------------"
+echo ">>> Installing oh-my-zsh"
+echo "--------------------------------------------------"
+sh oh-my-zsh.sh
+echo "DONE"
+
+
+echo ""
+echo "--------------------------------------------------"
+echo ">>> Installing other requirements"
+echo "--------------------------------------------------"
+# Installing Powerline and Powerline Fonts for ZSH
+apt-get install powerline fonts-powerline
+# Enable Syntax Highlighting on ZSH Shell
+apt-get install zsh-syntax-highlighting
+echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+sed -i "s/ZSH_THEME/# ZSH_THEME/" ~/.zshrc
+echo 'ZSH_THEME="af-magic"' >> ~/.zshrc
+echo "DONE
