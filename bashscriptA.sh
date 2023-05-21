bbccb14b89a7e52546aa23c888edc2f2a7dd3297
Mohammad Bayat <OkBayat@gmail.com>
2023-05-21 15:24:06 +0330

    Update and rename bashscript.sh to bashscriptA.sh

--- bashscriptA.sh
+++ bashscriptA.sh
@@ -34,17 +34,3 @@ echo ">>> Installing oh-my-zsh"
 echo "--------------------------------------------------"
 sh oh-my-zsh.sh
 echo "DONE"
-
-
-echo ""
-echo "--------------------------------------------------"
-echo ">>> Installing other requirements"
-echo "--------------------------------------------------"
-# Installing Powerline and Powerline Fonts for ZSH
-apt-get install powerline fonts-powerline
-# Enable Syntax Highlighting on ZSH Shell
-apt-get install zsh-syntax-highlighting
-echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-sed -i "s/ZSH_THEME/# ZSH_THEME/" ~/.zshrc
-echo 'ZSH_THEME="af-magic"' >> ~/.zshrc
-echo "DONE
