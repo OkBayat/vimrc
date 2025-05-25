b397aaa02f9225eedb9894d1a635c9f1af89d1cc
Mohammad Bayat <OkBayat@gmail.com>
2025-05-25 07:05:27 +0330

    Update bashscriptB.sh

--- bashscriptB.sh
+++ bashscriptB.sh
@@ -13,3 +13,21 @@ source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 sed -i "s/ZSH_THEME/# ZSH_THEME/" ~/.zshrc
 echo 'ZSH_THEME="af-magic"' >> ~/.zshrc
 echo "DONE
+
+
+echo ""
+echo "--------------------------------------------------"
+echo ">>> Enable vim mode in terminal
+echo "--------------------------------------------------"
+echo "DONE"
+BASHRC="$HOME/.zshrc"
+LINE="set -o vi"
+
+# Check if the line already exists
+if ! grep -Fxq "$LINE" "$BASHRC"; then
+  echo "$LINE" >> "$BASHRC"
+  echo "'$LINE' added to $BASHRC"
+else
+  echo "'$LINE' already exists in $BASHRC"
+fi
+echo "DONE"
